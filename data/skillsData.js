53d6d4cae0a2f12c3fc8ca727277839724ce2648
Richard Ogujawa <richardogujawa7@gmail.com>
2023-10-29 00:40:21 +0100

    added java and sql to skills page

--- data/skillsData.js
+++ data/skillsData.js
@@ -1,84 +1,92 @@
 const skillsData = [
-    {
-        'skill': 'React.js',
-        'img': '/img/react-js-logo.png',
-    },
-    {
-        'skill': 'Next.js',
-        'img': '/img/next-js-logo.svg',
-    },
-    {
-        'skill': 'React Native',
-        'img': '/img/react-native-logo.png',
-    },
-    {
-        'skill': 'TypeScript',
-        'img': '/img/typescript-logo.png',
-    },
-    {
-        'skill': 'MongoDB',
-        'img': '/img/mongo-db-logo.svg',
-    },
-    {
-        'skill': 'Firebase',
-        'img': '/img/firebase-logo.png',
-    },
-    {
-        'skill': 'Tailwind CSS',
-        'img': '/img/tailwind-logo.svg',
-    },
-     {
-        'skill': 'Redux',
-        'img': '/img/redux-logo.png',
-    },
-     {
-        'skill': 'Sanity.io',
-        'img': '/img/sanity-logo.png',
-    },
-    {
-        'skill': 'HTML',
-        'img': "/img/html-logo.svg",
-    },
-    {
-        'skill': 'CSS',
-        'img': "/img/css-logo.svg",
-    },
-    {
-        'skill': 'SASS',
-        'img': '/img/sass-logo.svg',
-    },
-    {
-        'skill': 'Javascript',
-        'img': '/img/js-logo.svg',
-    },
-    {
-        'skill': 'Github',
-        'img': '/img/github-logo.svg',
-    },
-    {
-        'skill': 'Postman',
-        'img': '/img/postman-logo.svg',
-    },
-    {
-        'skill': 'Adobe After Effects',
-        'img': '/img/aws-logo.svg',
-    },
-    {
-        'skill': 'Adobe Photoshop',
-        'img': '/img/ps-logo.svg',
-    },
-    {
-        'skill': 'Adobe Illustrator',
-        'img': '/img/ai-logo.svg',
-    },
-    {
-        'skill': 'Adobe XD',
-        'img': '/img/xd-logo.svg',
-    },
-    {
-        'skill': 'Jira',
-        'img': '/img/jira-logo.svg',
-    },
+  {
+    skill: 'React.js',
+    img: '/img/react-js-logo.png',
+  },
+  {
+    skill: 'Next.js',
+    img: '/img/next-js-logo.svg',
+  },
+  {
+    skill: 'React Native',
+    img: '/img/react-native-logo.png',
+  },
+  {
+    skill: 'TypeScript',
+    img: '/img/typescript-logo.png',
+  },
+  {
+    skill: 'MongoDB',
+    img: '/img/mongo-db-logo.svg',
+  },
+  {
+    skill: 'Firebase',
+    img: '/img/firebase-logo.png',
+  },
+  {
+    skill: 'Tailwind CSS',
+    img: '/img/tailwind-logo.svg',
+  },
+  {
+    skill: 'Redux',
+    img: '/img/redux-logo.png',
+  },
+  {
+    skill: 'Sanity.io',
+    img: '/img/sanity-logo.png',
+  },
+  {
+    skill: 'HTML',
+    img: '/img/html-logo.svg',
+  },
+  {
+    skill: 'CSS',
+    img: '/img/css-logo.svg',
+  },
+  {
+    skill: 'SASS',
+    img: '/img/sass-logo.svg',
+  },
+  {
+    skill: 'Javascript',
+    img: '/img/js-logo.svg',
+  },
+  {
+    skill: 'Github',
+    img: '/img/github-logo.svg',
+  },
+  {
+    skill: 'Postman',
+    img: '/img/postman-logo.svg',
+  },
+  {
+    skill: 'Adobe After Effects',
+    img: '/img/aws-logo.svg',
+  },
+  {
+    skill: 'Adobe Photoshop',
+    img: '/img/ps-logo.svg',
+  },
+  {
+    skill: 'Adobe Illustrator',
+    img: '/img/ai-logo.svg',
+  },
+  {
+    skill: 'Adobe XD',
+    img: '/img/xd-logo.svg',
+  },
+  {
+    skill: 'Jira',
+    img: '/img/jira-logo.svg',
+  },
+  {
+    skill: 'Java',
+    img: '/img/java-logo.svg',
+  },
+  {
+    skill: 'SQL',
+    img: '/img/sql-logo.png',
+  },
 ]
 
 export default skillsData
